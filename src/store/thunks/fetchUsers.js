3af711c7bb3591292a54307a4bb5ee274956ff5c
Naman <hikom563@gmail.com>
2024-04-26 17:15:38 +0530

    first commit

---
+++ src/store/thunks/fetchUsers.js
@@ -0,0 +1,24 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const fetchUsers = createAsyncThunk("users/fetch", async () => {
+  const response = await axios.get("http://localhost:3005/users");
+
+
+  // DEV ONLY!!
+  await pause(1000)
+  // DEV ONLY!!
+
+
+  return response.data;
+});
+
+// DEV ONLY!!!
+
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
+
+export { fetchUsers };
